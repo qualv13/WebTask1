4baddd295e945dbe8aa4030537e4221468bd3834
qualv13 <71326761+qualv13@users.noreply.github.com>
2024-07-06 19:50:32 +0200

    a rób to na PC a nie jakimś lapku

--- index.js
+++ index.js
@@ -8,43 +8,46 @@
 // let d = new Date();
 // document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 
-(function () {
+window.onload = function () {
     const DELIMITER = ',';
     const NEWLINE = '\n';
     const DATA = document.getElementById('file'); //'data.csv'
     const table = document.getElementById('table'); //'table'
     //const DATA2 =
-    if (!DATA){
+    const path = "./Data.csv";
+    const DATA2 = getFile(path).file()
+    if (!DATA) {
         return
     }
 
-    DATA.addEventListener('change', function (){
-        if(!!DATA.files && DATA.files.length > 0){
-            parseCSV(DATA.files[0]);
-        }
-    })
+    // DATA.addEventListener('change', function () {
+    //     if (!!DATA.files && DATA.files.length > 0) {
+    //         parseCSV(DATA.files[0]);
+    //     }
+    // })
+    parseCSV(DATA.files[0]);
 
 
-
-    function parseCSV(file){
-        if (!file || !FileReader){
+    function parseCSV(file) {
+        if (!file || !FileReader) {
             return;
         }
         let reader = new FileReader();
 
-        reader.onload = function (e){
+        reader.onload = function (e) {
             toTable(e.target.result);
         };
 
         reader.readAsText(file);
 
     }
-    function toTable(text){
-        if(!text||!table){
+
+    function toTable(text) {
+        if (!text || !table) {
             return;
         }
 
-        while (table.lastElementChild){
+        while (table.lastElementChild) {
             table.removeChild(table.lastElementChild);
         }
 
@@ -56,7 +59,7 @@
             let th = document.createElement('th');
             let ht = h.trim();
 
-            if(!ht){
+            if (!ht) {
                 return;
             }
 
@@ -68,21 +71,21 @@
 
         let rtr;
 
-        rows.forEach(function (r){
+        rows.forEach(function (r) {
             r = r.trim();
-            if (!r){
+            if (!r) {
                 return;
             }
 
             let cols = r.split(DELIMITER);
 
-            if (cols.length === 0){
+            if (cols.length === 0) {
                 return;
             }
 
             rtr = document.createElement('tr')
 
-            cols.forEach(function (c){
+            cols.forEach(function (c) {
                 let td = document.createElement('td');
                 //let tc = c.trim();
 
@@ -93,6 +96,95 @@
             table.appendChild(rtr);
         });
 
-
     }
-})();
+}
+
+//
+// (function () {
+//     const DELIMITER = ',';
+//     const NEWLINE = '\n';
+//     const DATA = document.getElementById('file'); //'data.csv'
+//     const table = document.getElementById('table'); //'table'
+//     //const DATA2 =
+//     if (!DATA){
+//         return
+//     }
+//
+//     DATA.addEventListener('change', function (){
+//         if(!!DATA.files && DATA.files.length > 0){
+//             parseCSV(DATA.files[0]);
+//         }
+//     })
+//
+//
+//
+//     function parseCSV(file){
+//         if (!file || !FileReader){
+//             return;
+//         }
+//         let reader = new FileReader();
+//
+//         reader.onload = function (e){
+//             toTable(e.target.result);
+//         };
+//
+//         reader.readAsText(file);
+//
+//     }
+//     function toTable(text){
+//         if(!text||!table){
+//             return;
+//         }
+//
+//         while (table.lastElementChild){
+//             table.removeChild(table.lastElementChild);
+//         }
+//
+//         let rows = text.split(NEWLINE);
+//         let headers = rows.shift().split(DELIMITER);
+//         let htr = document.createElement('tr');
+//
+//         headers.forEach(function (h) {
+//             let th = document.createElement('th');
+//             let ht = h.trim();
+//
+//             if(!ht){
+//                 return;
+//             }
+//
+//             th.textContent = ht;
+//             htr.appendChild(th);
+//         });
+//
+//         table.appendChild(htr);
+//
+//         let rtr;
+//
+//         rows.forEach(function (r){
+//             r = r.trim();
+//             if (!r){
+//                 return;
+//             }
+//
+//             let cols = r.split(DELIMITER);
+//
+//             if (cols.length === 0){
+//                 return;
+//             }
+//
+//             rtr = document.createElement('tr')
+//
+//             cols.forEach(function (c){
+//                 let td = document.createElement('td');
+//                 //let tc = c.trim();
+//
+//                 td.textContent = c.trim();
+//                 rtr.appendChild(td);
+//             });
+//
+//             table.appendChild(rtr);
+//         });
+//
+//
+//     }
+// })();
